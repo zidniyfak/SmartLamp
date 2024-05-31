ecc8ece7b86d7e31833dcc60f9a262e165bd5fab
Zidniy Alkausar <suratalkausar@gmail.com>
2024-05-31 21:11:59 +0700

    Login Firebase

--- app/src/main/java/com/example/smart_lamp/LoginActivity.kt
+++ app/src/main/java/com/example/smart_lamp/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.smart_lamp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
-
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,8 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -32,6 +35,36 @@ class LoginActivity : AppCompatActivity() {
 
 
         binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            // Validation
+            if (email.isEmpty()) {
+                binding.etEmail.error = "Please enter your email"
+                return@setOnClickListener
+            }
+            else if (password.isEmpty()) {
+                binding.etPassword.error = "Please enter your password"
+                return@setOnClickListener
+            }
+
+            // Email Validation
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.etEmail.error = "Please enter a valid email address"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            // Password Validation
+            if(password.length <6) {
+                binding.etPassword.error = "Password must be greater than 6 characters"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            RegisterFirebase(email,password)
+
+            // Get the email and password from the EditText fields
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
@@ -40,5 +73,19 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun RegisterFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Selamat Datang : $email", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
